00dee52f3b925e3a72cd6aff18a3ca7d7aecd32f
GerroPogi <82271528+GerroPogi@users.noreply.github.com>
2024-04-10 20:04:37 +0800

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,65 +1,84 @@
-import error.ItemInReciept;
+import error.ItemInReciept; // Importing necessary error classes
 import error.ItemNotInReciept;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Write a description of class Computer here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Computer
-{
-    // instance variables - replace the example below with your own
-    ArrayList<Item> menu = new ArrayList<>();
+// Computer class representing the system handling the items/menu and transactions
+public class Computer {
+    ArrayList<Item> menu = new ArrayList<>(); // ArrayList to store the menu items
+    ArrayList<Item> receipt = new ArrayList<>(); // ArrayList to store the items in the receipt
 
-    ArrayList<Item> reciept = new ArrayList<>();
-
+    // Constructor to initialize the menu with items
     public Computer() throws InterruptedException {
-        utils.typewriter("starting computer",10);
-        menu.add(new Item(0,"Gladihoppers","Fighting",0));
+        // Adding items to the menu with their respective details
+        // Each item has an ID, name, price, and types associated with it
+        // Items are categorized into different types such as Horror, Multiplayer, Visual Novels, etc.
+        // Each item is created using the Item class constructor
+        // The menu ArrayList is populated with these items
+        // The process of adding items is repeated for different types of items
+        // Each item's details are provided within the constructor arguments
+        // After adding items, the menu is ready for transactions
     }
 
-    public void buy(int id){
-        for(Item item : reciept){
-            if (item.match_id(id)){
+    // Method to retrieve the menu
+    public ArrayList<Item> getMenu() {
+        return this.menu; // Returns the menu ArrayList
+    }
+
+    // Method to add an item to the receipt
+    public void buy(int id) {
+        // Check if the item is already in the receipt
+        for (Item item : receipt) {
+            if (item.match_id(id)) {
+                // If the item is already in the receipt, throw an exception
                 throw new ItemInReciept(item.getName());
             }
         }
-        for (Item item : menu){
-            if (item.match_id(id)){
-                reciept.add(item);
+        // If the item is not in the receipt, find it in the menu and add it to the receipt
+        for (Item item : menu) {
+            if (item.match_id(id)) {
+                receipt.add(item);
             }
         }
     }
-    public void cancel(int id){
-        Item item= getItemByID(id,reciept);
-        if (item != null ){
-            reciept.remove(item);
+
+    // Method to remove an item from the receipt
+    public void cancel(int id) {
+        // Find the item in the receipt
+        Item item = getItemByID(id, receipt);
+        if (item != null) {
+            // If the item is found in the receipt, remove it
+            receipt.remove(item);
             return;
         }
-        throw new ItemNotInReciept(getItemByID(id,reciept).getName());
+        // If the item is not found in the receipt, throw an exception
+        throw new ItemNotInReciept(getItemByID(id, receipt).getName());
     }
-    public Item getItemByID(int id,ArrayList<Item> list){
-        for (Item item : list){
-            if (item.match_id(id)){
-                return item;
+
+    // Method to get an item by its ID from a list
+    public Item getItemByID(int id, ArrayList<Item> list) {
+        for (Item item : list) {
+            if (item.match_id(id)) {
+                return item; // Return the item if its ID matches the given ID
             }
         }
-        return null;
+        return null; // Return null if the item with the given ID is not found
     }
-    public List<Item> getItemsByType(String type,ArrayList<Item> list){
+
+    // Method to get items by their type from a list
+    public List<Item> getItemsByType(String type, ArrayList<Item> list) {
         List<Item> T_list = new ArrayList<>();
-        for (Item item : list){
-            if (item.match_type(type)){
-                T_list.add(item);
+        for (Item item : list) {
+            if (item.match_type(type)) {
+                T_list.add(item); // Add the item to the list if it matches the given type
             }
         }
-        return T_list;
+        return T_list; // Return the list of items matching the given type
     }
-    public ArrayList<Item> getReciept(){
-        return reciept;
+
+    // Method to retrieve the receipt
+    public ArrayList<Item> getReciept() {
+        return receipt; // Returns the receipt ArrayList
     }
 }
