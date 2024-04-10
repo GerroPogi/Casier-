2cd240a5fb58b0112fe56f33225f2a0fa8a6586c
GerroPogi <82271528+GerroPogi@users.noreply.github.com>
2024-04-10 20:06:35 +0800

    Update ItemInReciept.java

--- error/ItemInReciept.java
+++ error/ItemInReciept.java
@@ -1,5 +1,7 @@
 package error;
-
+/*
+    A runtime error to check if an item is in the reciept
+ */
 public class ItemInReciept extends RuntimeException{
     public ItemInReciept(String ItemName){
         super("Item name: "+ItemName+" already in reciept");
